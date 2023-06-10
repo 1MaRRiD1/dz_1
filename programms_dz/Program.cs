83bec88bab6861f7b1a60658d53c5b2e1c706935
Maksim <norten1156@gmail.com>
2023-06-10 15:57:28 +0300

    хочу попробовать без цикла

--- programms_dz/Program.cs
+++ programms_dz/Program.cs
@@ -2,7 +2,7 @@
 //int number = Convert.ToInt32(Console.ReadLine());
 // 1 задача  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.Write("Введите первое число: ");
+/*Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
@@ -13,4 +13,30 @@ if ( number1>number2){
 else {
     Console.WriteLine( $"число {number2} наибольшее" ) ;
     Console.WriteLine( $"число {number1} наименьшее" ) ;
+}*/
+
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите первое число: ");
+  int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+  int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье  число: ");
+  int number3 = Convert.ToInt32(Console.ReadLine());
+int count = 0 ;
+int max = number1 ;
+while(count<2){
+    if(max<number2){
+        max = number2;
+    }
+    else if(max<number3){
+        max= number3;
+    }
+    else{
+        count++ ;
+    }
 }
+Console.Write($"{max} - наибольшее из трех чисел ");
+
+
+
+
