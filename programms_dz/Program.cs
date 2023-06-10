dc23a6e73fda44ecd049ebeddeaeebda0f2522c1
Maksim <norten1156@gmail.com>
2023-06-10 16:44:55 +0300

    решил крайнюю задачу

--- programms_dz/Program.cs
+++ programms_dz/Program.cs
@@ -47,4 +47,12 @@ if (number%2==0){
 else{
   Console.Write($"число {number} является нечетным");
 }*/
+// задача 4Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
+/*Console.Write("N= ");
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i=1;i<=n;i++)
+{
+if (i % 2 == 0)
+System.Console.Write(i + ",");
+}*/
